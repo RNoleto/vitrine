b380c884dde8e8ce49309d3e8eab77009dd41671
Ramon Noleto <devnoleto@gmail.com>
2025-04-17 14:22:29 -0300

    fix: Ajuste de nome dos componentes.

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../components/views/Home.vue'
 import Stores from '../components/views/Stores.vue'
 import Contacts from '../components/views/Contacts.vue'
-import StoreDetail from '../components/views/StoreDetail.vue'
-import StoreContacts from '../components/views/StoreContacts.vue'
+import StorePage from '../components/views/StorePage.vue'
+import StoreContactsPage from '../components/views/StoreContactsPage.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
     { path: '/stores', name: 'Stores', component: Stores },
     { path: '/contacts', name: 'Contacts', component: Contacts },
-    { path: '/stores/:id', name: 'StoreDetail', component: StoreDetail },
-    { path: '/contacts/:id/contacts', name: 'StoreContacts', component: StoreContacts },
+    { path: '/stores/:id', name: 'StorePage', component: StorePage },
+    { path: '/contacts/:id/contacts', name: 'StoreContactsPage', component: StoreContactsPage },
 ]
 
 const router = createRouter({
