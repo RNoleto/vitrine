f54b78bb57c6e78f26b329e704dd732eb042db13
Ramon Noleto <devnoleto@gmail.com>
2025-04-18 14:28:15 -0300

    chore: Ajuste de alias no vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     vue(),
     tailwindcss()
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   server: {
     historyApiFallback: true
   }
