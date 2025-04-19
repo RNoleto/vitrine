0c1e0917f6ec1c4e9f0072ceb1f16f5012ca2e41
Ramon Noleto <devnoleto@gmail.com>
2025-04-19 19:40:03 -0300

    Ajuste de layout da lista de lojas na home

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -11,13 +11,13 @@ onMounted(() => {
 
 <template>
     <section>
-        <p>Minhas Lojas</p>
-        <div class="mt-2">
+        <p class="font-bold">Minhas Lojas</p>
+        <div class="mt-4">
             <div v-if="lojaStore.carregando">Carregando lojas...</div>
             <div v-else-if="lojaStore.erro" class="text-red-500">{{ lojaStore.erro }}</div>
             <div v-else-if="lojaStore.lojas.length === 0">Nenhuma loja cadastrada</div>
-            <div v-else v-for="(loja, index) in lojaStore.lojas" :key="index" class="flex items-center gap-2 space-y-2">
-                <div class="flex items-center gap-3">
+            <div v-else v-for="(loja, index) in lojaStore.lojas" :key="index" class="p-2 border border-indigo-600  rounded-md shadow-sm mb-2">
+                <div class="flex items-center gap-2">
                     <img :src="loja.logo_url" alt="logo" class="w-8 h-8 object-contain" />
                     <span class="font-medium">{{ loja.name }}</span>
                 </div>
